49e68332f703935c09599f4e031c394280efe84f
Abhinav Tiwary <abhinavbuisness2005@gmail.com>
2026-07-25 09:57:40 +0530

    fix: direct server.js export in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const serverApp = require('../server.js');
-
-const app = express();
-app.use('/api', serverApp);
-app.use('/', serverApp);
-
-module.exports = app;
+module.exports = require('../server.js');
