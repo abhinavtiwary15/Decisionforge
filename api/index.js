087f906ae4e44776d0399dbb8d79cb472acc283f
Abhinav Tiwary <abhinavbuisness2005@gmail.com>
2026-07-25 10:20:28 +0530

    fix: mount serverApp under /api and / in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,8 @@
-const app = require('../server.js');
+const express = require('express');
+const serverApp = require('../server.js');
+
+const app = express();
+app.use('/api', serverApp);
+app.use('/', serverApp);
+
 module.exports = app;
